2639c9b4f9899368f6ef3758c71a587ba5723bf1
munozmau <mau.munoz18@gmail.com>
2018-06-15 12:13:09 -0700

    Complete full_name_greeting exercise

---
+++ week1/ch05/full_name_greeting.rb
@@ -0,0 +1,8 @@
+puts 'Hello there, and what\'s your first name?'
+first = gets.chomp + ' '
+puts 'And what\'s your middle name?'
+middle = gets.chomp + ' '
+puts 'And what\'s your last name?'
+last = gets.chomp
+puts 'Your name is ' + first + middle + last + '? What a lovely name!'
+puts 'Pleased to meet you, ' + first + middle + last +'. :)' 
