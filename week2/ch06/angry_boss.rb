bafcb2439909211263cef24f905d47ce9b5a63fa
munozmau <mau.munoz18@gmail.com>
2018-06-29 01:43:25 -0700

    Complete angry_boss exercise

---
+++ week2/ch06/angry_boss.rb
@@ -0,0 +1,11 @@
+annoyed = 'oh joy! another pest.'
+selfish = 'what could you possibly want that is'
+boss = 'important enough to disturb my precious day?'
+puts annoyed.upcase
+puts selfish.upcase
+puts boss.upcase
+request = gets.chomp
+puts request.capitalize
+puts 'EXCUSE ME? WHAT DO YOU MEAN ' + '"' + request.upcase + '"?!?!'
+puts 'AND YOU HAVE THE AUDACITY TO SAY THIS IN MY PRESENCE?!'
+puts 'YOU\'RE FIRED!!!'
